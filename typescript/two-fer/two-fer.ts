34ce87d6a17c790a1edce801241adbcccd5f3a38
João Henrique Serodio <joao.serodio@vnator.com>
2023-07-29 14:01:45 -0300

    ts: two fer

---
+++ typescript/two-fer/two-fer.ts
@@ -0,0 +1,7 @@
+/**
+ * This stub is provided to make it straightforward to get started.
+ */
+
+export function twoFer(friend: string = "you"): string {
+  return `One for ${friend}, one for me.`;
+}
