0592c3bc560e9b6402d366320784a13aa6e98336
João Henrique Serodio <joao.serodio@vnator.com>
2023-08-30 16:55:11 -0300

    feat: update elixir exercice

--- elixir/lasagna/lib/lasagna.ex
+++ elixir/lasagna/lib/lasagna.ex
@@ -1,14 +1,15 @@
 defmodule Lasagna do
-  def preparation_time_for_layer do 2 end
+  @preparation_time_for_layer 2
+  @expected_minutes_in_oven 40
 
-  def expected_minutes_in_oven do 40 end
+  def expected_minutes_in_oven(), do: @expected_minutes_in_oven
 
   def remaining_minutes_in_oven(pastedMinutes) do
     expected_minutes_in_oven() - pastedMinutes
   end
 
   def preparation_time_in_minutes(layers) do
-    preparation_time_for_layer() * layers
+    @preparation_time_for_layer * layers
   end
 
   # Please define the 'total_time_in_minutes/2' function
@@ -17,5 +18,5 @@ defmodule Lasagna do
   end
 
   # Please define the 'alarm/0' function
-  def alarm do "Ding!" end
+  def alarm(), do: "Ding!"
 end
