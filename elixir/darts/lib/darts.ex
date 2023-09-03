00716ad35b13d51d7cf6196d3024699a632f4bc3
João Henrique Serodio <joao.serodio@vnator.com>
2023-09-03 19:51:51 -0300

    feat: refactor darts in elixir track

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -1,16 +1,19 @@
 defmodule Darts do
   @type position :: {number, number}
-
   @doc """
   Calculate the score of a single dart hitting a target
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    lands = :math.sqrt(
+      (x*x) + (y*y)
+    )
+
     cond do
-      (x > 10 or x < 0) and (y > 10 or x < 0) -> 0
-      (x <= 10 and x > 5) and (y <= 10 and y > 5) -> 1
-      (x <= 5 or x < 1) and (y <= 5 or y < 1) -> 5
-      (x < 1) and (y < 1) -> 10
+      lands > 10 -> 0
+      lands > 5 -> 1
+      lands > 1 -> 5
+      true -> 10
     end
   end
 end
