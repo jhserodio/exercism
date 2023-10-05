a5cf9804940ee7c6140680df01d487d59f0043b6
João Henrique Serodio <joao.serodio@vnator.com>
2023-10-05 13:08:13 -0300

    track in Rust exercism Gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,11 +1,6 @@
-use time::{PrimitiveDateTime as DateTime, Duration, offset::Utc};
+use time::{PrimitiveDateTime as DateTime, Duration};
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    let start_milli = start.millisecond();
-    let now_milli = DateTime::now().millisecond();
-
-    let duration = Duration::from_millis(now_milli - start_milli);
-    
-    PrimitiveDateTime::from_unix_duration(duration)
+    start + Duration::seconds(1_000_000_000)
 }
